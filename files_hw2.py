2c51c36415cd1a135859aa8b8417f57c3940345d
Angelina Koroleva <angvkoroleva@gmail.com>
2025-06-28 17:22:23 +0300

    Second task

---
+++ files_hw2.py
@@ -0,0 +1,49 @@
+import os
+import pprint
+
+
+file_path = os.path.join(os.getcwd(), 'recipes.txt')
+cook_book = {}
+
+with open(file_path, 'r', encoding='UTF-8') as f:
+    while True:
+        # Чтение названия блюда
+        dish_name = f.readline().strip()
+
+        if not dish_name:
+            break
+
+        # Чтение количества ингредиентов
+        ingredient_count = int(f.readline().strip())   
+
+        # Чтение самих ингредиентов
+        ingredients = []
+        for _ in range(ingredient_count):
+            ingredient_line = f.readline().strip()
+            ingredient, quantity, unit = ingredient_line.split(' | ')
+            ingredients.append({
+                    'ingredient': ingredient,
+                    'quantity': int(quantity),
+                    'unit': unit
+                })
+            
+        # Добавление блюда в словарь
+        cook_book[dish_name] = ingredients
+            
+        # Пропуск пустой строки между рецептами
+        f.readline()
+
+def get_shop_list_by_dishes(dishes, person_count):
+    recipes = {}
+    for dish in dishes:
+        for dish_name in cook_book:
+            if dish == dish_name:
+                for recipe in cook_book[dish_name]:
+                    # print(recipe)
+                    recipe['quantity'] *= person_count
+                    recipes.setdefault(recipe['ingredient'], \
+                        {'measure': recipe['unit'], 'quantity': recipe['quantity']})         
+    return pprint.pprint(recipes)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)      
+            
