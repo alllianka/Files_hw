ab553c9502d09f863a01f4d3f292eb5557649218
Angelina Koroleva <angvkoroleva@gmail.com>
2025-07-01 23:08:27 +0300

    Solved third task

--- files_hw3.py
+++ files_hw3.py
@@ -10,20 +10,18 @@ def files_combiner(input_files, output_file):
     info = {}
 
     for infile in input_files:
-        strings = len(open(infile, 'r', encoding='utf-8').readlines())
-        info.setdefault(strings, infile)
-        sorted_info = dict(sorted(info.items()))
-        with open(infile, 'r+', encoding='utf-8') as infile:    
-            content = infile.read()
-            infile.seek(0,0)
-            infile.write(infile.name+'\n'+str(strings)+'\n')
-            infile.write(content)
-    
-    with open(output_file, 'w', encoding='utf-8') as outfile:
-        for key, infile in sorted_info.items():
-            with open(infile, 'r', encoding='utf-8') as infile:
-                outfile.write(infile.read())
-                outfile.write('\n')
+        with open(infile, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            num_lines = len(lines)
+            info[infile] = (num_lines, lines)  
+
+    sorted_files = sorted(info.items(), key=lambda x: x[1][0])      
+    with open(infile, 'w', encoding='utf-8') as outfile:    
+        for infile, (num_lines, content) in sorted_files:
+            outfile.write(f"{infile}\n")
+            outfile.write(f"{num_lines}\n")
+            outfile.writelines(content)
+            outfile.write("\n")
          
 files_combiner([file1, file2, file3], output_file)
 
